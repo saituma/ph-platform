9787ef06ae254c32b943f8a5d765d732aa554f66
Dawit <dawitworkujima@gmail.com>
2026-04-09 21:42:30 +0300

    feat: add Select component import to TeamMemberDetailPage

--- apps/web/app/teams/[teamName]/members/[athleteId]/page.tsx
+++ apps/web/app/teams/[teamName]/members/[athleteId]/page.tsx
@@ -13,6 +13,7 @@ import {
 } from "../../../../../components/ui/card";
 import { Input } from "../../../../../components/ui/input";
 import { Label } from "../../../../../components/ui/label";
+import { Select } from "../../../../../components/ui/select";
 import { Textarea } from "../../../../../components/ui/textarea";
 import { SectionHeader } from "../../../../../components/admin/section-header";
 
