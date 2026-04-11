7105708bbaf6a76e406dbf8598f9a35991228026
Dawit <dawitworkujima@gmail.com>
2026-04-11 11:45:11 +0300

    feat(nutrition): add youth snack slots

---
+++ apps/api/drizzle/0074_nutrition_snack_slots.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "nutrition_logs" ADD COLUMN "snacksMorning" text;
+ALTER TABLE "nutrition_logs" ADD COLUMN "snacksAfternoon" text;
+ALTER TABLE "nutrition_logs" ADD COLUMN "snacksEvening" text;
+
